21151bfd0bf358d8373ee296c7d123ccfe69d0d7
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-10-29 21:36:28 -0400

    Chore: add buttons

---
+++ src/components/Primarybutton.jsx
@@ -0,0 +1,3 @@
+export default function PrimaryButton({ text }) {
+    return <button className='px-4 py-2 bg-[#ffd803] text-[#272343] rounded-lg font-bold'>{text}</button>
+} 
---
+++ src/components/SecondaryButton.jsx
@@ -0,0 +1,3 @@
+export default function SecondaryButton({ text }) {
+    return <button className='px-4 py-2 bg-sky-600 rounded-lg'>{text}</button>
+} 
