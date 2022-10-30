7ed6c7340afa4ba1a7a9cdb5879386eccd567e75
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-10-29 21:44:35 -0400

    Docs: document code

--- src/pages/api/cars/[id].js
+++ src/pages/api/cars/[id].js
@@ -1,39 +1,44 @@
 import Car from '@/models/Car'
 import { dbConnection } from '@/utils/mongoose'
+import { connection } from 'mongoose'
+
+dbConnection()
 
 export default async (req, res) => {
 
-    const { method, body, query: { id } } = req
-    const { status } = res
+    const { method, body, query: { id } } = req //destructuring req methods
+    const { status } = res //destructuring res methods
+
+    /* switcher for method for an specific id (car) */
 
     switch (method) {
-        case 'GET':
+        case 'GET': //get method
             try {
-                const car = await Car.findById(id)
-                if (!car) return status(404).json({ msg: 'This car can not be found' })
+                const car = await Car.findById(id) //.findById() => fucntion that search a item based in its id
+                if (!car) return status(404).json({ msg: 'This car can not be found' }) // if no car return a response with status code an a message
                 return status(200).json(car)
             } catch (error) {
                 return status(500).json({ msg: error.message })
             }
-        case 'UPDATE':
+        case 'UPDATE': //update method
             try {
-                const updatedCar = await Car.findByIdAndUpdate(id, body, {
-                    new: true
+                const updatedCar = await Car.findByIdAndUpdate(id, body, { //.findByIdAndUpdate() => function that search a item based in its id and update it
+                    new: true //for returning the updated version of the item
                 })
-                if (!updatedCar) return status(404).json({ msg: 'This car can not be found' })
+                if (!updatedCar) return status(404).json({ msg: 'This car can not be found' }) // if no car return a response with status code an a message
                 return status(200).json(updatedCar)
             } catch (error) {
                 return status(500).json({ msg: error.message })
             }
-        case 'DELETE':
+        case 'DELETE': //delete method
             try {
-                const deletedCar = await Car.findByIdAndDelete(id)
-                if (!deletedCar) return status(404).json({ msg: 'This car can not be found' })
+                const deletedCar = await Car.findByIdAndDelete(id) //.findByIdAndDelete() => function that search a item based in its id and delete it
+                if (!deletedCar) return status(404).json({ msg: 'This car can not be found' }) // if no car return a response with status code an a message
                 return status(204).json({ msg: 'Car deleted' })
             } catch (error) {
                 return status(400).json({ msg: error.message })
             }
         default:
-            return status(400).json('This methos is not supported')
+            return status(400).json('This methos is not supported') // default response
     }
 }
--- src/pages/api/cars/index.js
+++ src/pages/api/cars/index.js
@@ -1,24 +1,34 @@
 import Car from '@/models/Car'
 import { dbConnection } from '@/utils/mongoose'
+import { connection } from 'mongoose'
 
-dbConnection()
+dbConnection() //importing connection from mongoose.js
 
 export default async function handler(req, res) {
 
-    const { method, body } = req
-    const { status } = res
+    const { method, body } = req //destructuring req methods
+    const { status } = res //destructuring res methods
+
+    //switcher for methods
 
     switch (method) {
-        case 'GET':
-            const car = await Car.find()
-            return status(200).json(car)
-        case 'POST':
-            const newCar = new Car(body)
-            const savedCar = await newCar.save()
-            return status(201).json(savedCar)
-
+        case 'GET': //get method
+            try {
+                const car = await Car.find() //function that search in collection by .find() function and assigned to car variable
+                return status(200).json(car) // response returned with an status code and a json object with car variable
+            } catch (err) {
+                return status(404).json({ msg: err.message }) //for errors returns an status code and a json object with an error message
+            }
+        case 'POST': //post method
+            try {
+                const newCar = new Car(body) // generate an instance of Car from models and with a body assigned to newcar variable
+                const savedCar = await newCar.save() // wait for .save() function and assign to savedCar variable 
+                return status(201).json(savedCar)
+            } catch (err) {
+                return status(400).json({ msg: err.message })
+            }
         default:
-            return status(400).json({ msg: 'this method is not supported' })
+            return status(400).json({ msg: 'this method is not supported' }) // default response
 
     }
 
