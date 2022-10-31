d817974ef279ef1b559bc4de1fbaeb893848ef09
Santiago Hernandez <santiagooheernandez@gmail.com>
2022-10-30 21:48:53 -0400

    Feat: add more info to car model

--- src/models/Car.js
+++ src/models/Car.js
@@ -4,13 +4,32 @@ import { model, models, Schema } from 'mongoose'
 
 const carSchema = new Schema({
     name: String,
-    description: String,
-    price: Number,
-    age: String,
-    km: Number,
     location: String,
-    more: String,
-    image: String
+    price: Number,
+    description: String,
+    image: String,
+    details: {
+        odometer: Number,
+        age: String,
+        bodyStyle: String,
+        color: String,
+        engineType: String,
+        cylinders: Number,
+        transmission: String,
+        fuel: String,
+        highlights: String,
+        owners: Number,
+        wheels: String,
+        ac: Boolean,
+        interior: String,
+        interiorColor: String,
+        more: String
+    },
+    info: {
+        createdAt: Date,
+        updatedAt: Date,
+        createdBy: String
+    }
 })
 
 // if car model exist export it, if it does not exist create a new one and exportit
